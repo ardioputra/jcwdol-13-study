2dafad8efad82ca6d18fc430026bfa1c64693521
ardioputra <ardioputra.1109@gmail.com>
2024-02-05 21:00:01 +0700

    build: new file session 4

---
+++ Session4/index.js
@@ -0,0 +1,22 @@
+// Array
+
+let sepatu1 = "Adidas";
+let sepatu2 = "Nike";
+let sepatu3 = "Puma";
+
+let listSepatu = ["Adidas","Nike","Puma"];
+console.log(listSepatu[2]);
+console.log(listSepatu.length);
+
+sepatu1 = "NB";
+console.log(sepatu1);
+
+listSepatu[0] = "NB";
+listSepatu[3] = "Adidas";
+listSepatu.push("Sneakers")
+
+for(let i = 0; i<listSepatu.length; i++){
+    console.log(listSepatu[i])
+}
+
+let listAngka = [1,2,3];
