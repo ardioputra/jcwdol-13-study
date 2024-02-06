920e6f6694055c528b7d2b34df695dab80db27c1
ardioputra <ardioputra.1109@gmail.com>
2024-02-06 21:55:42 +0700

    build: adding arrow function example

---
+++ Session4/arrowFunc.js
@@ -0,0 +1,13 @@
+const pertambahan = (a,b) => {
+    return a + b;
+}
+
+const pengurangan = (a,b) => {
+    return a - b;
+}
+
+const pengurangan1 = (a,b) => a - b;
+
+console.log(pertambahan(2,2))
+console.log(pengurangan(2,2))
+console.log(pengurangan1(2,3))
